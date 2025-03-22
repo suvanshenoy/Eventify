f2cade4614664c9f90a4ccc5a632608154b1f675
Surabhi2206 <118377652+Surabhi2206@users.noreply.github.com>
2025-03-22 09:14:14 +0530

    Add files via upload

---
+++ admin-panel/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li><Link to="/event-organizers">Event Organizers</Link></li>
+        <li><Link to="/attendees">Attendees</Link></li>
+        <li><Link to="/attendees">Events</Link></li>
+
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
